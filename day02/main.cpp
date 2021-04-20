bb0aca4c8b1aaa5d0d74c65dddff5fbfdbdaa34a
Astrid Delcros <astriddelcros@gmail.com>
2021-04-20 23:41:09 +0200

    fixed point numbers init

---
+++ day02/main.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adelcros <adelcros@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/01 10:59:27 by astriddel         #+#    #+#             */
+/*   Updated: 2021/04/02 03:30:31 by astriddel        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "Fixed.hpp"
+
+int	main(void)
+{
+	Fixed	a;
+	Fixed	b(a);
+	Fixed	c;
+	
+	c = b;
+	std::cout << a.getRawBits() << std::endl; std::cout << b.getRawBits() << std::endl; std::cout << c.getRawBits() << std::endl;
+	return 0;
+}
