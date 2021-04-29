90856ee70c6fd23b24a752cb7406a47ddd79cc19
Astrid Delcros <astriddelcros@gmail.com>
2021-04-30 01:23:28 +0200

    norme

--- day02/ex01/Fixed.hpp
+++ day02/ex01/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: adelcros <adelcros@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/01 10:59:27 by astriddel         #+#    #+#             */
-/*   Updated: 2021/04/02 03:30:31 by astriddel        ###   ########.fr       */
+/*   Updated: 2021/04/30 01:22:50 by astriddel        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,23 @@
 # include <iostream>
 # include <cmath>
 
-class       Fixed {
+class   Fixed {
     
 public:
     Fixed();
-	Fixed(Fixed const &fixed);
+    Fixed(Fixed const &fixed);
     Fixed(int const intValue);
     Fixed(float const floatValue);
     ~Fixed();
-	Fixed&  operator=(Fixed const &fixed);
-	int     getRawBits(void) const;
-	void    setRawBits(int const raw);
+    Fixed&  operator=(Fixed const &fixed);
+    int     getRawBits(void) const;
+    void    setRawBits(int const raw);
     float   toFloat(void) const;
     int     toInt(void) const;
 
-private:
-	int     _value;
-    static const int     _fractionalBits = 8;
+    private:
+    int     _value;
+    static const int    _fractionalBits = 8;
     
 };
 
