37a75d7b5fa7be7ea5faabce17b33d2bc7bbfed4
Astrid Delcros <astriddelcros@gmail.com>
2021-06-13 09:49:15 +0200

    main tests

--- day04/ex00/main.cpp
+++ day04/ex00/main.cpp
@@ -16,12 +16,33 @@
 #include <iostream>
 #include <cstdlib>
 
-int main() {
+void schoolTests() {
     Sorcerer    robert("Robert", "the Magnificent");
     Victim      jim("Jimmy");
     Peon        joe("Joe");
     std::cout << robert << jim << joe;
     robert.polymorph(jim);
     robert.polymorph(joe);
+}
+
+void homemadeTests() {
+    Sorcerer    *merlin = new Sorcerer("Merlin", "the Wizard");
+    std::cout << *merlin;
+    std::cout << "A thief pretends to be Merlin.\n";
+    Sorcerer    *thief(merlin);
+    Victim      traveller("traveller");
+    thief->polymorph(traveller);
+    Victim      traveller2("traveller2");
+    traveller2 = traveller;
+    std::cout << traveller2;
+    traveller2.getPolymorphed();
+    delete thief;
+}
+
+int main() {
+    std::cout << "\033[0;31mSchool Tests\033[0m" << std::endl;
+    schoolTests();
+    std::cout << "\033[0;31mHome-made Tests\033[0m" << std::endl;
+    homemadeTests();
     return 0;
 }
